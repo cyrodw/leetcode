d97536f8a25b425b5e9c0ffa24880e46c096e599
ben <bwoo2718@gmail.com>
2023-05-11 16:25:52 +1000

    commit

---
+++ python/minimum-cost-of-a-path-with-special-roads.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minimumCost(self, start: List[int], target: List[int], specialRoads: List[List[int]]) -> int:
+        cost = lambda x, y : abs(x[0] - y[0]) + abs(x[1] - y[1]) 
+        
+        specialRoads = [start + start + [0]] + specialRoads 
+        dist = [float('inf') for _ in specialRoads] 
+        dist[0] = 0 
+        vis = [False for _ in specialRoads]
+          
+        res = cost(start, target)  
+        pq = [(0, 0)] 
+        while pq: 
+            d, v = heappop(pq)
+            if vis[v]:
+                continue
+            vis[v] = True
+                
+            res = min(res, d + cost(specialRoads[v][2:4], target))
+            for w in range(len(dist)):
+                if vis[w]:
+                    continue   
+
+                alt = d + cost(specialRoads[v][2:4], specialRoads[w][:2]) + specialRoads[w][4]
+                if alt < dist[w]:
+                    dist[w] = alt 
+                    heappush(pq, (alt, w))
+            
+        return res 
+            
+        
